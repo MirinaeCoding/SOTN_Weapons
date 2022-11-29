51f62a4ed7acae44cdaf598594f9cf5a422a2fe0
Clara <c.garcia@cfa-insta.fr>
2022-11-29 21:43:19 +0100

    css + images

--- php/home.php
+++ php/home.php
@@ -1,4 +1,4 @@
-<?php @require "../php/database.php" ?>
+<?php //@require "../php/database.php" ?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -7,25 +7,56 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <title>Castlevania SOTN - Weapons</title>
 </head>
 
 <body>
 
-    <input type="text" placeholder="Search">
+<div class="table-wrapper">
+    <table class="fl-table">
+        <tr>
+            <th>Image</th>
+            <th>Name</th>
+            <th>Type</th>
+            <th>Attributes</th>
+            <th>Statistics</th>
+            <th>Found</th>
+            <th>Drop</th>
+            <th>Notes</th>
+            <th>Specials</th>
+        </tr>
 
-    <br>
+        <tr>
+            <td><img src="../images/short_swords/basilard.webp" alt="" width="50px"></td>
+            <td><b>Basilard</b></td>
+            <td>Short Sword</td>
+            <td>Cut</td>
+            <td>ATT+1</td>
+            <td>Alchemy Laboratory</td>
+            <td>Bloody Zombie (0.81%)</td>
+            <td>Overhand Stab</td>
+            <td>←→ + [Attack] for Horizontal Knife Slash (ATT +5)</td>
+        </tr>
 
-    <table>
-        <tr>Image</tr>
-       <tr>Type</tr>
-       <tr>Name</tr>
-       <tr>Attributes</tr>
-       <tr>Statistics</tr>
-       <tr>Found</tr>
-       <tr>Droprate</tr>
-       <tr>Notes</tr>
+        <tr>
+            <td><img src="../images/short_swords/were_bane.webp" alt="" width="50px"></td>
+            <td><b>Were Bane</b></td>
+            <td>Short Sword</td>
+            <td>Cut</td>
+            <td>ATT+18</td>
+            <td>-</td>
+            <td>Hunting Girl (0.11%)</td>
+            <td>Stronger against were beasts (Werewolf, Minotaur)</td>
+            <td>↓↘→ + [Attack] for continuous attack (ATT +12)</td>
+        </tr>
+        
+
+        </table>
+</div>
+
+
+    
     </table>
 
 </body>
